36cc910617ca1a4894185924f6b3b65611fc6dd2
vinutha91 <vinu.raju@gmail.com>
2019-08-29 11:52:13 +0530

    Removing Index.js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+console.log('Hii');
+
+addItem('Hii');
+
 function addItem(data: any) {
     var node = document.createElement('li');
     var textNode = document.createTextNode(data);
     node.appendChild(textNode);
-    document.getElementById('output').appendChild(node);
+    document.getElementById('list').appendChild(node);
 }
